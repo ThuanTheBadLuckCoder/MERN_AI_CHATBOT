f04d9550251a586a44f8656ce11292471baba888
Tran Thuan <dtthuan.1211@gmail.com>
2024-09-02 15:03:50 +0700

    user login/signup backend
    1:38:00, first commit - no API key from chatgpt

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,13 +1,9 @@
-import express from 'express';
-const app = express();
-// GET - 
-// PUT -
-// POST - 
-// DELETE -
-app.use(express.json());
-app.delete("/hello", (req, res, next) => {
-    console.log(req.body.name);
-    return res.send("hello");
-});
-app.listen(5000, () => console.log("Server Open"));
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
+// connections and listeneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log(`Server Open & Connected to Database at ${PORT}`));
+}).catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express'
-
-const app = express();
-
-// middlewares
-app.use(express.json());
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
 
 
-// connection and listeneres
-app.listen(5000, () => console.log("Server Open"));
+// connections and listeneres
+const PORT = process.env.PORT || 5000
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Open & Connected to Database at ${PORT}`));
+}).catch((err) => console.log(err));
