20a7f1b53e18d9a7135a2067bfe014d0e1ef419b
Tran Thuan <dtthuan.1211@gmail.com>
2024-09-20 17:09:15 +0700

    get to understand
    nothing muchhhhhhhhhhhh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,11 @@ import app from './app.js';
 import { connectToDatabase } from './db/connection.js';
 
 
+
 // connections and listeneres
 const PORT = process.env.PORT || 5000
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () => console.log(`Server Open & Connected to Database at ${PORT}`));
 }).catch((err) => console.log(err));
+
