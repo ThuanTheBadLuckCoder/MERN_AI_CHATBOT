16ab63ab60ec0d05b81eb7d913f491399530bf89
Tran Thuan <dtthuan.1211@gmail.com>
2024-12-05 15:13:41 +0700

    Update index.ts
    delete all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import app from './app.js';
-import { model } from './config/gemini-config.js';
 import { connectToDatabase } from './db/connection.js';
 
 
@@ -10,11 +9,3 @@ connectToDatabase()
 .then(() => {
   app.listen(PORT, () => console.log(`Server Open & Connected to Database at ${PORT}`));
 }).catch((err) => console.log(err));
-
-
-try {
-  
-
-} catch (error) {
-  console.log(error);
-}
